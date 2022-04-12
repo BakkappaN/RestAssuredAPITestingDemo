a17cd34a9e6d301ab400537f1bbd7a37d677a888
BakkappaN <bakkappac@gmail.com>
2022-04-12 16:41:45 +0530

    updated schema validation

--- RestAssuredAPITestingFramework/src/test/java/com/testautomation/tests/SchemaValidation.java
+++ RestAssuredAPITestingFramework/src/test/java/com/testautomation/tests/SchemaValidation.java
@@ -1,35 +1,34 @@
 package com.testautomation.tests;
 
-import java.net.http.HttpResponse.BodyHandler;
-
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import com.google.gson.JsonArray;
-import com.jayway.jsonpath.JsonPath;
-
-import io.restassured.RestAssured.*;
+import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.RestAssured;
-//import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import net.bytebuddy.NamingStrategy.SuffixingRandom.BaseNameResolver.ForGivenType;
-import net.minidev.json.JSONArray;
-import net.minidev.json.JSONObject;
 
 public class SchemaValidation {
 
 	@Test
 	public void schemaValidationTest() {
 		// add maven dependency <artifactId>json-schema-validator</artifactId>
-		// use to conver json to json schema -
+		// use to convert json to json schema -
 		// https://www.liquid-technologies.com/online-json-to-schema-converter
 
-		RestAssured.get("https://reqres.in/api/users?page=2")
+		//Given
+		RestAssured
+			.given()
+				.baseUri("https://reqres.in/api/users?page=2")
+				.contentType(ContentType.JSON)
+		
+		//When
+		.when()
+			.get()
+		
+		//Then
 		.then()
-		.assertThat()
-		.statusCode(200)
-		.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemavalidationfile.json"));
+			.assertThat()
+			.statusCode(200)
+			.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schemavalidationfile.json"));
 	}
 
 }
